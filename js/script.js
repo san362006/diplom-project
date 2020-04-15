e261540fb690c060a17c4e562991d2f1d331c71f
san362006 <san362006@rambler.ru>
2020-04-15 10:04:47 +0400

    Корректировка форматирования кода

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(function(){
   let $body = $('body');
-	let $container = $('.modal-container');
+  let $container = $('.modal-container');
   let $modal = $('.modal-callback');
   let $mobileContainer = $('.header__nav-mobile');
-	let $mobileMenu = $('.nav__list-mobile');
-	let $mobileControls = $('.mobile-controls');
+  let $mobileMenu = $('.nav__list-mobile');
+  let $mobileControls = $('.mobile-controls');
 
-  
+
   //Плавный переход по якорной ссылке
   $('a[href^="#"]').on('click', function(evt) {
     evt.preventDefault();
@@ -16,19 +16,18 @@ $(function(){
     $('html, body').animate({scrollTop: position}, 1000);
   });
 
-  
-	// Функция отключения скролла
-	function disableScroll() {
+  // Функция отключения скролла
+  function disableScroll() {
     let pagePosition = window.scrollY;
     $body.addClass('fixed');
     $body.attr('data-position', pagePosition);
     $body.css({
       top: -pagePosition + 'px'
     })
-	};
+  };
 
-	// Функция включения скролла
-	function enableScroll() {
+  // Функция включения скролла
+  function enableScroll() {
     let pagePosition = parseInt($body.attr('data-position'), 10);
     $body.css({
       top: 'auto'
@@ -36,65 +35,65 @@ $(function(){
     $body.removeClass('fixed');
     window.scrollTo(0, pagePosition)
     $body.removeAttr('data-position');
-	}
+  }
 
   //Функция показа мобильного меню
-	function showMenu() {
+  function showMenu() {
     $mobileContainer.addClass('nav-mobile-open');
     $mobileMenu.animate({left: 0});
-		$mobileControls.toggleClass('hamburger');
+    $mobileControls.toggleClass('hamburger');
     $mobileControls.toggleClass('close');
     disableScroll();
-	}
+  }
 
-	//Функция скрытия мобильного меню
-	function hideMenu() {
-		$mobileContainer.removeClass('nav-mobile-open');
-		$mobileMenu.animate({left: '-320px'});
-		$mobileControls.toggleClass('close');
+  //Функция скрытия мобильного меню
+  function hideMenu() {
+    $mobileContainer.removeClass('nav-mobile-open');
+    $mobileMenu.animate({left: '-320px'});
+    $mobileControls.toggleClass('close');
     $mobileControls.toggleClass('hamburger');
     enableScroll();
-	}
-  
+  }
+
   // Функция закрытия MODAL
   function closeModal (){
     $container.fadeOut(400, enableScroll);
-		$modal.animate({
-			top: '40%'
-		})
+    $modal.animate({
+      top: '40%'
+    })
   }
 
   //Открываем-закрываем мобильное меню
-	$mobileControls.click(function(){
-		if (this.className == 'mobile-controls hamburger') {
-			showMenu();
-		} else {
+  $mobileControls.click(function(){
+    if (this.className == 'mobile-controls hamburger') {
+      showMenu();
+    } else {
       hideMenu();
-		}
-	});
+    }
+  });
 
-	//Закрываем меню по клику на пункт мобильного меню
-	$('.nav__link-mobile').click(function(){
-		if ($mobileControls.css('display') !== 'none'){
-			hideMenu();
-		}
-	});
+  //Закрываем меню по клику на пункт мобильного меню
+  $('.nav__link-mobile').click(function(){
+    if ($mobileControls.css('display') !== 'none'){
+      hideMenu();
+    }
+  });
 
   // Открываем MODAL
   $('.modal-open').click(function(evt){
     evt.preventDefault();
-		$container.fadeIn(400, disableScroll);
-		$modal.animate({
-		top: '50%'
-		})
-	});
+    $container.fadeIn(400, disableScroll);
+    $modal.animate({
+      top: '50%'
+    })
+  });
 
-	// Закрываем MODAL по клику на пустой области
-	$container.click(function(evt){
-		if(evt.target == this) {
-			$(this).fadeOut();
-			closeModal();
-		}
+  // Закрываем MODAL по клику на пустой области
+  $container.click(function(evt){
+    if(evt.target == this) {
+      $(this).fadeOut();
+      closeModal();
+    }
   });
 
   // Закрываем MODAL по клику по кнопке закрыть
@@ -104,15 +103,14 @@ $(function(){
 
   // Маска ввода номера телефона
   $('input[type="tel"]').inputmask({"mask": "+7 (999) 999-99-99"});
-  
-  
+
   // Валидация формы
   $('form').each(function () {
     $(this).validate({
       errorPlacement(error, element) {
         return true;
       },
-      
+
       focusInvalid: false,
       rules: {
         Телефон: {
@@ -122,7 +120,7 @@ $(function(){
           required: true,
         }
       },
-      
+
       submitHandler(form) {
         let th = $(form);
 
@@ -141,19 +139,18 @@ $(function(){
   });
 });
 
-
 //Инициализация слайдера
 var mySwiper = new Swiper('.swiper-container', {
   speed: 500,
   slidesPerView: 3,
   spaceBetween: 30,
   loop: 'true',
-  
+
   navigation: {
     nextEl: '.swiper__control_arrow-right',
     prevEl: '.swiper__control_arrow-left',
   },
-  
+
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
@@ -172,6 +169,4 @@ var mySwiper = new Swiper('.swiper-container', {
       loop: 'false'
     }
   },
-
 });
-
